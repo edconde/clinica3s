26504682b15aa10a2e8957f35a131d81e6223666
edconde <15130843+edconde@users.noreply.github.com>
2025-12-31 14:06:31 +0100

    Crear esqueleto backend vacío

---
+++ clinica3s-backend/src/main/java/io/github/edconde/clinica3s_backend/Clinica3sBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.edconde.clinica3s_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clinica3sBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clinica3sBackendApplication.class, args);
+	}
+
+}
---
+++ clinica3s-backend/src/test/java/io/github/edconde/clinica3s_backend/Clinica3sBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.edconde.clinica3s_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clinica3sBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
