dc50d09b6112d62034aa6d29b576cf2c19aadba9
edconde <15130843+edconde@users.noreply.github.com>
2026-01-02 13:48:27 +0100

    incluir fuentes en dist

--- clinica3s-frontend/vite.config.js
+++ clinica3s-frontend/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/primeicons/fonts/*',
+          dest: 'assets/fonts'
+        }
+      ]
+    })
+  ],
+  build: {
+    assetsInlineLimit: 0,
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        charset: false
+      }
+    }
+  }
 })
